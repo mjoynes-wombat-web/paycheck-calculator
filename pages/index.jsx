4494a791b120a787466c14b3066da7ee1ec89948
ssmith-wombatweb <ssmith@wombatweb.us>
2018-09-14 16:53:54 -0700

    Setup data for form elements.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 import camelCase from 'camelcase';
+import usStates from 'datasets-us-states-abbr';
 
 import MainTemplate from './layout/main';
 import Progress from './components/progress';
 
-class Step {
-  constructor(text, label) {
+class Input {
+  constructor(text, label, pattern = '') {
     this.id = camelCase(text);
     this.text = text;
     this.label = label;
+    this.value = null;
+    this.pattern = pattern;
     this.complete = false;
+  }
+}
+
+class NumInput extends Input {
+  constructor(text, label, pattern, step = 0, min = 1, max = null) {
+    super(text, label, pattern);
+    this.type = 'number';
+    this.step = step;
+    this.min = min;
+    this.max = max;
+  }
+}
+
+class SelectInput extends Input {
+  constructor(text, label, pattern, options) {
+    super(text, label, pattern);
+    this.options = options;
   }
 }
 
@@ -18,12 +38,12 @@ class Index extends Component {
     super();
     this.state = {
       steps: [
-        new Step('Pay Rate', 'How much do you make an hour?'),
-        new Step('Hours', 'How many hours are in this paycheck?'),
-        new Step('Filing Status', 'What is your filing status?'),
-        new Step('US State', 'What state do you live in?'),
-        new Step('Pay Frequency', 'How often do you get paid?'),
-        new Step('Exemptions', 'What number of exemptions do you get?'),
+        new NumInput('Pay Rate', 'How much do you make an hour?', 0.01),
+        new NumInput('Hours', 'How many hours are in this paycheck?'),
+        new SelectInput('Filing Status', 'What is your filing status?', ['Single', 'Married', 'Married Filing Separately', 'Head of Household']),
+        new SelectInput('US State', 'What state do you live in?', usStates),
+        new SelectInput('Pay Frequency', 'How often do you get paid?', ['Weekly', 'Bi-Weekly', 'Semi-Monthly', 'Monthly', 'Quarterly', 'Yearly']),
+        new NumInput('Exemptions', 'What number of exemptions do you get?', 0, 0, 20),
       ],
       currentStep: 0,
     };
