c68636cdd8485e04263f9e6c3568bca950d4c892
ssmith-wombatweb <ssmith@wombatweb.us>
2018-09-14 16:20:35 -0700

    Progress bar setup.

--- pages/components/progress.jsx
+++ pages/components/progress.jsx
@@ -1,24 +1,97 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Progress = ({ steps, changeActiveStep }) => (
-  <ul>
-    {steps.map(step => (
-      <li key={step.id}>
-        <button type="button" onClick={() => changeActiveStep(step.id)}>
-          {step.text}
-        </button>
-      </li>
-    ))}
-  </ul>
-);
+import colors from '../../consts/colors';
+
+const Progress = ({ steps, changeActiveStep, currentStep }) => {
+  const completion = ((currentStep / steps.length) - (0.5 / steps.length)) * 100;
+  return (
+    <nav className="progress">
+      <style jsx>
+        {`
+          span.progress-bar {
+            display: inline-block;
+            color: transparent;
+            width: 100%;
+            transform-origin: left;
+            transition: transform 0.5s;
+            background-color: ${colors.vividGreen()};
+            line-height: 0;
+            height: 0.125rem;
+            position: absolute;
+          }
+          ul {
+            display: flex;
+            padding: 0;
+            list-style: none;
+            justify-content: space-around;
+            color: white;
+            border-top: 0.125rem solid white;
+            transition: border-image-source 0.5s;
+
+            li {
+              flex: 1;
+              display: flex;
+              align-content: center;
+              flex-direction: column;
+              position: relative;
+
+              :before {
+                content: "•";
+                text-align: center;
+                position: relative;
+                line-height: 0;
+                font-size: 2rem;
+                top: -0.0625rem;
+                transition: color 0.5s linear 0.5s;
+              }
+
+              button {
+                background-color: transparent;
+                border: none;
+                color: white;
+                padding: 1rem;
+                transition: color 0.5s linear 0.5s;
+                font-size: 1.25rem;
+              }
+
+              &.complete {
+                :before, button {
+                  color: ${colors.vividGreen()}
+                }
+              }
+            }
+          }
+          `}
+      </style>
+      <span
+        className="progress-bar"
+        style={{
+          transform: `scaleX(${completion / 100})`,
+        }}
+      >
+        {completion}% Done
+      </span>
+      <ul>
+        {steps.map(step => (
+          <li key={step.id} className={step.complete ? 'complete' : ''}>
+            <button type="button" onClick={() => changeActiveStep(step.id)}>
+              {step.text}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
 Progress.propTypes = {
   steps: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number,
+    id: PropTypes.text,
     text: PropTypes.text,
   })).isRequired,
   changeActiveStep: PropTypes.func.isRequired,
+  currentStep: PropTypes.number.isRequired,
 };
 
 export default Progress;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import camelCase from 'camelcase';
 
 import MainTemplate from './layout/main';
 import Progress from './components/progress';
 
 class Step {
-  constructor(id, text, label) {
-    this.id = id;
+  constructor(text, label) {
+    this.id = camelCase(text);
     this.text = text;
     this.label = label;
+    this.complete = false;
   }
 }
 
@@ -16,29 +18,22 @@ class Index extends Component {
     super();
     this.state = {
       steps: [
-        new Step('payRate', 'Pay Rate', 'How much do you make an hour?'),
-        {
-          id: 'hours',
-          text: 'Hours',
-          label: 'How many hours are in this paycheck?',
-        },
-        {
-          id: 'filingStatus',
-          text: 'Filing Status',
-          label: 'What is your filing status?',
-        },
-        {
-
-        },
+        new Step('Pay Rate', 'How much do you make an hour?'),
+        new Step('Hours', 'How many hours are in this paycheck?'),
+        new Step('Filing Status', 'What is your filing status?'),
+        new Step('US State', 'What state do you live in?'),
+        new Step('Pay Frequency', 'How often do you get paid?'),
+        new Step('Exemptions', 'What number of exemptions do you get?'),
       ],
+      currentStep: 0,
     };
   }
 
   render() {
-    const { steps } = this.state;
+    const { steps, currentStep } = this.state;
     return (
       <MainTemplate>
-        <Progress steps={steps} />
+        <Progress steps={steps} currentStep={currentStep} />
       </MainTemplate>
     );
   }
