00c42690856491457140f4a4da13bb6c7a33afb0
ssmith-wombatweb <ssmith@wombatweb.us>
2018-09-16 08:46:47 -0700

    Changed form to stateless component function.

--- pages/components/form/index.jsx
+++ pages/components/form/index.jsx
@@ -1,71 +1,59 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import NumberInput from './inputs/number';
 import SelectInput from './inputs/select';
 import Confirmation from './confirmation';
 
-class Form extends Component {
-  constructor() {
-    super();
-    this.state = {
-      submit: false,
-    };
-  }
-
-  render() {
-    const {
-      steps, currentStep, nextStep, changeValue, changeActiveStep, submitForm,
-    } = this.props;
-    return (
-      <form>
-        <style jsx>
-          {`
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          `}
-        </style>
-        {Object.keys(steps).map((stepName, i) => {
-          switch (steps[stepName].constructor.name) {
-            case 'NumInput':
-              return (
-                <NumberInput
-                  key={steps[stepName].id}
-                  {...steps[stepName]}
-                  currentStep={currentStep}
-                  index={i}
-                  nextStep={nextStep}
-                  changeValue={changeValue}
-                  lastInput={(Object.keys(steps).length - 1) === i}
-                />
-              );
-            case 'SelectInput':
-              return (
-                <SelectInput
-                  key={steps[stepName].id}
-                  {...steps[stepName]}
-                  currentStep={currentStep}
-                  index={i}
-                  nextStep={nextStep}
-                  changeValue={changeValue}
-                  lastInput={(Object.keys(steps).length - 1) === i}
-                />
-              );
-            default:
-              return null;
-          }
-        })}
-        <Confirmation
-          steps={steps}
-          currentStep={currentStep}
-          changeActiveStep={changeActiveStep}
-          submitForm={submitForm}
-        />
-      </form>
-    );
-  }
-}
+const Form = ({
+  steps, currentStep, nextStep, changeValue, changeActiveStep, submitForm,
+}) => (
+  <form>
+    <style jsx>
+      {`
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      `}
+    </style>
+    {Object.keys(steps).map((stepName, i) => {
+      switch (steps[stepName].constructor.name) {
+        case 'NumInput':
+          return (
+            <NumberInput
+              key={steps[stepName].id}
+              {...steps[stepName]}
+              currentStep={currentStep}
+              index={i}
+              nextStep={nextStep}
+              changeValue={changeValue}
+              lastInput={(Object.keys(steps).length - 1) === i}
+            />
+          );
+        case 'SelectInput':
+          return (
+            <SelectInput
+              key={steps[stepName].id}
+              {...steps[stepName]}
+              currentStep={currentStep}
+              index={i}
+              nextStep={nextStep}
+              changeValue={changeValue}
+              lastInput={(Object.keys(steps).length - 1) === i}
+            />
+          );
+        default:
+          return null;
+      }
+    })}
+    <Confirmation
+      steps={steps}
+      currentStep={currentStep}
+      changeActiveStep={changeActiveStep}
+      submitForm={submitForm}
+    />
+  </form>
+);
 
 Form.propTypes = {
   steps: PropTypes.objectOf(PropTypes.object).isRequired,
