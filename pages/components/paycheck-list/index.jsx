c7da04c705193ba22ce0389e178406941529f7aa
ssmith-wombatweb <ssmith@wombatweb.us>
2018-09-16 14:38:12 -0700

    Fixed issue with history counting for width of element.

--- pages/components/paycheck-list/index.jsx
+++ pages/components/paycheck-list/index.jsx
@@ -7,77 +7,88 @@ import colors from '../../../consts/colors';
 const PaycheckList = ({
   paychecks, open, toggleMenu, clickedOnce,
 }) => (
-  <nav className={open ? 'open' : ''}>
+  <div className="wrapper">
     <style jsx>
       {`
-      nav {
-        color: white;
-        position: absolute;
-        right: 0;
+      div.wrapper {
+        position: fixed;
+        width: 100vw;
+        height: 100vh;
         top: 0;
-        padding: 2rem;
-        background-color: ${colors.midnightNavy(0.95)};
-        min-height: 100%;
+        left: 0;
+        overflow: hidden;
         z-index: 100;
-        border-left: 0.125rem solid rgba(255, 255, 255, 0.10);
-        transform: translateX(100%);
-        transition: transform 0.5s;
-        box-sizing: border-box;
+        pointer-events: none;
 
-        &.open {
-          transform: translateX(0);
-        }
-
-        button {
-          @keyframes pulse {
-            0% { transform: translateX(-100%); };
-            50% { transform: scale(1.1) translateX(-100%); };
-            100% { transform: translateX(-100%); };
-          }
-          transform: translateX(-100%);
-          box-sizing: border-box;
+        nav {
+          color: white;
           position: absolute;
-          padding: 1rem;
-          left: 0;
+          right: 0;
           top: 0;
-          background-color: transparent;
-          border: none;
-          color: ${colors.vividGreen()};
+          padding: 2rem;
+          background-color: ${colors.midnightNavy(0.95)};
+          min-height: 100%;
+          border-left: 0.125rem solid rgba(255, 255, 255, 0.10);
+          transform: translateX(100%);
           transition: transform 0.5s;
-          font-family: Raleway, sans-serif;
-          transform-origin: top right;
-          font-size: 1.25rem;
-          animation-name: pulse;
-          animation-duration: 1.5s;
-          animation-iteration-count: infinite;
+          box-sizing: border-box;
+          pointer-events: all;
 
-          :focus, :hover {
-            outline: none;
-            transform: scale(1.1) translateX(-100%);
+          &.open {
+            transform: translateX(0);
           }
 
-          &.clicked {
-            animation-name: none;
+          button {
+            @keyframes pulse {
+              0% { transform: translateX(-100%); };
+              50% { transform: scale(1.1) translateX(-100%); };
+              100% { transform: translateX(-100%); };
+            }
+            transform: translateX(-100%);
+            box-sizing: border-box;
+            position: absolute;
+            padding: 1rem;
+            left: 0;
+            top: 0;
+            background-color: transparent;
+            border: none;
+            color: ${colors.vividGreen()};
+            transition: transform 0.5s;
+            font-family: Raleway, sans-serif;
+            transform-origin: top right;
+            font-size: 1.25rem;
+            animation-name: pulse;
+            animation-duration: 1.5s;
+            animation-iteration-count: infinite;
+
+            :focus, :hover {
+              outline: none;
+              transform: scale(1.1) translateX(-100%);
+            }
+
+            &.clicked {
+              animation-name: none;
+            }
           }
-        }
-        article {
-          table {
-            th {
-              vertical-align: bottom;
-              font-family: 'Raleway', sans-serif;
-              font-size: 1.25rem;
-              font-weight: normal;
-            }
-            th, :global(td) {
-              padding: 0.5rem 0.75rem;
-              text-align: left;
-            }
-            th:not(:last-child):not(:nth-last-child(2)), :global(td:not(:last-child):not(:nth-last-child(2))) {
-              border-right: 0.125rem solid rgba(255, 255, 255, 0.25);
-            }
-            th.not-mobile, :global(td.not-mobile) {
-              @media screen and (max-width: 750px) {
-                display: none;
+          article {
+            table {
+              th {
+                vertical-align: bottom;
+                font-family: 'Raleway', sans-serif;
+                font-size: 1.25rem;
+                font-weight: normal;
+              }
+              th, :global(td) {
+                padding: 0.5rem 0.75rem;
+                text-align: left;
+              }
+              th:not(:last-child):not(:nth-last-child(2)), :global(td:not(:last-child):not(:nth-last-child(2))) {
+                border-right: 0.125rem solid rgba(255, 255, 255, 0.25);
+              }
+              th.not-mobile, :global(td.not-mobile) {
+                @media screen and (max-width: 750px) {
+                  display: none;
+                }
               }
             }
           }
@@ -85,32 +96,34 @@ const PaycheckList = ({
       }
       `}
     </style>
-    {paychecks.length ? <button className={clickedOnce ? 'clicked' : ''} type="button" onClick={toggleMenu}>{open ? 'X' : 'History'}</button> : null }
-    <article className="list">
-      {paychecks.length ? (
-        <>
-          <h2>Paychecks</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>Hourly Wage</th>
-                <th>Hours</th>
-                <th className="not-mobile">Filing Status</th>
-                <th className="not-mobile">US State</th>
-                <th className="not-mobile">Pay Frequency</th>
-                <th className="not-mobile">Exemptions</th>
-                <th>Net</th>
-                <th />
-              </tr>
-            </thead>
-            <tbody>
-              {paychecks.map(paycheck => <PaycheckItem key={paycheck.id} paycheck={paycheck} />)}
-            </tbody>
-          </table>
-        </>
-      ) : <h2>No Paychecks</h2>}
-    </article>
-  </nav>
+    <nav className={open ? 'open' : ''}>
+      {paychecks.length ? <button className={clickedOnce ? 'clicked' : ''} type="button" onClick={toggleMenu}>{open ? 'X' : 'History'}</button> : null }
+      <article className="list">
+        {paychecks.length ? (
+          <>
+            <h2>Paychecks</h2>
+            <table>
+              <thead>
+                <tr>
+                  <th>Hourly Wage</th>
+                  <th>Hours</th>
+                  <th className="not-mobile">Filing Status</th>
+                  <th className="not-mobile">US State</th>
+                  <th className="not-mobile">Pay Frequency</th>
+                  <th className="not-mobile">Exemptions</th>
+                  <th>Net</th>
+                  <th />
+                </tr>
+              </thead>
+              <tbody>
+                {paychecks.map(paycheck => <PaycheckItem key={paycheck.id} paycheck={paycheck} />)}
+              </tbody>
+            </table>
+          </>
+        ) : <h2>No Paychecks</h2>}
+      </article>
+    </nav>
+  </div>
 );
 
 PaycheckList.propTypes = {
--- pages/components/paycheck/index.jsx
+++ pages/components/paycheck/index.jsx
@@ -13,8 +13,8 @@ const Paycheck = ({ check, paycheckReceived, closePaycheck }) => (
       .wrapper {
         color: white;
         position: fixed;
-        width: 100%;
-        height: 100%;
+        width: 100vw;
+        height: 100vh;
         top: 0;
         left: 0;
         display: flex;
