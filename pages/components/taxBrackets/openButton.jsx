5438c0c2f32ff4de5cc4182688e73a36ff53e93e
ssmith-wombatweb <ssmith@wombatweb.us>
2018-09-16 21:45:58 -0700

    Added styling to view tax brackets button.

--- pages/components/taxBrackets/openButton.jsx
+++ pages/components/taxBrackets/openButton.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import colors from '../../../consts/colors';
+
 const OpenTaxBrackets = ({ toggleTaxBrackets }) => (
   <button type="button" onClick={toggleTaxBrackets}>
-      View Tax Brackets
+    <style jsx>
+      {`
+      button {
+        background-color: transparent;
+        border: none;
+        color: ${colors.vividGreen()};
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        padding: 1rem;
+        font-size: 1.25rem;
+        transition: transform 0.5s;
+        font-family: Raleway,sans-serif;
+        transform-origin: bottom left;
+
+        :hover, :focus {
+          outline: none;
+          transform: scale(1.1);
+        }
+      }
+      `}
+    </style>
+    View Tax Brackets
   </button>
 );
 
